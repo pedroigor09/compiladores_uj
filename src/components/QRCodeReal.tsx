801cb538df16e20c45da90664abea4418a27c6c3
pedroigor09 <peuigor09@gmail.com>
2025-09-16 21:34:05 -0300

    Fix: Implement REAL scannable QR Code using qrcode library

--- src/components/QRCodeReal.tsx
+++ src/components/QRCodeReal.tsx
@@ -37,8 +37,6 @@ export default function QRCodeComponent({ url }: QRCodeComponentProps) {
     }
   }
 
-
-
   const copyToClipboard = async () => {
     try {
       await navigator.clipboard.writeText(siteUrl)
